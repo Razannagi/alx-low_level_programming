f3d8d2389c088a5ea7797acf0b85546daf540ecb
Razannagi <Razannagi557@gmail.com>
2023-09-07 18:22:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
-	* main - Entry point
-	*
-	* Description: Calls puts and recieves value returned by puts after its
-	* execution.
-	* Return: Always return 0 to indicate success
-*/
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
-	 return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
